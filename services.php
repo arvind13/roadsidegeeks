33456ce9f21d90ad43300773e07456c496f75761
Pulkit <pulkitkumar190@gmail.com>
2015-08-12 19:18:28 +0530

    added 2 new pages
    added 2 new pages
    and some css in main.css

---
+++ services.php
@@ -0,0 +1,37 @@
+<?php include 'layout/header.php' ?>
+
+<div class = "cover">
+	<div class="body-holder">
+		<center><h1>SERVICES</h1></center>
+	</div>
+
+	<div class="body-holder">
+		<p style="text-align:center;"><strong>List of Services provided by us.</strong></p>
+	</div>
+
+<!-- Services Cards -->
+<div class="card-outer">
+	<div class="card">
+		<div class="card-holder">
+			<h1 style="color:black;text-align:center;"> Web Development</h1>
+			<p class="card-para">We provide best Web Development Solution's at a affordable rate </p>
+		</div>
+	</div>
+
+	<div class="card" style="background-color:#FFC107">
+		<div class="card-holder">
+			<h1 style="color:black;text-align:center;"> Image Editing</h1>
+			<p class="card-para">We provide Best Image Editing at a Affordable rate.</p>
+		</div>
+	</div>
+
+	<div class="card" style="background-color:#B2DFDB">
+		<div class="card-holder">
+			<h1 style="color:black;text-align:center;"> SEO </h1>
+			<p class="card-para">We provide On-Site and Off-Site SEO at a affordable rate</p>
+		</div>
+	</div>
+</div>
+</div><!-- End of cover -->
+
+<?php include 'layout/footer.php' ?>
