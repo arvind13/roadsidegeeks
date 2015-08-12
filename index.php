859f863f1756126296548c1db2b0102eb928cf8a
Arvind Singh <desiboyarvind@gmail.com>
2015-08-11 17:48:38 -0700

    Initial files added

---
+++ index.php
@@ -0,0 +1,24 @@
+<?php include 'layout/header.php'; ?>
+<div class="cover">
+	<div class="body-holder">
+		<center syle="padding:100px 0px;">
+			<h1 style="">OUR CLIENT'S SUCCESS IS OUR SUCCESS</h1>
+		<p style="">Welcome To The Leading Website Design And Development Company In Patiala, Punjab.</p>
+		</center>
+	</div>
+</div>
+<div class="body-holder">
+	<h1>Why Us</h1><hr>
+	<p>We provide best service in punjab and whole india</p>
+	<span style="float:right"><img src="/img/responsive-design.jpg"></span>
+</div>
+
+
+<div class="body-holder">
+	<h1>Why Us</h1><hr>
+	<p>We provide best service in punjab and whole india</p>
+</div>
+
+
+
+<?php include 'layout/footer.php'; ?>
---
+++ layout/header.php
@@ -0,0 +1,36 @@
+<!doctype html>
+<html class="no-js" lang="">
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="x-ua-compatible" content="ie=edge">
+        <title>RoadSideGeeks.com</title>
+        <meta name="description" content="">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <link rel="apple-touch-icon" href="apple-touch-icon.png">
+        <!-- Place favicon.ico in the root directory -->
+
+        <link rel="stylesheet" href="/css/normalize.css">
+        <link rel="stylesheet" href="/css/main.css">
+        <script src="/js/vendor/modernizr-2.8.3.min.js"></script>
+    </head>
+    <body>
+        <!--[if lt IE 8]>
+            <p class="browserupgrade">You are using an <strong>outdated</strong> browser. Please <a href="http://browsehappy.com/">upgrade your browser</a> to improve your experience.</p>
+        <![endif]-->
+        <div class="menu">
+            <div class="body-holder">
+                <a class="active" href="">HOME</a><a href="">SERVICES</a><a href="">PRICING</a><a href="">WORK</a><a href="">CONTACT</a>
+                <span class="right">
+                    <a href="">+91 (720) 626 4120</a>
+                </span>
+            </div>
+        </div>
+        <header>
+            <div class="body-holder">
+                <img src="/img/logo.png">
+
+            </div>
+        </header>
+        
+        
